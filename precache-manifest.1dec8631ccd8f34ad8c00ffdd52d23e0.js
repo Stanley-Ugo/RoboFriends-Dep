78b062ff581da164d129cbf57cea2a8829c6ab45
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-13 00:24:00 +0100

    Deployed folder to RoboFriends App

---
+++ precache-manifest.1dec8631ccd8f34ad8c00ffdd52d23e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e0a138a8d30ee8e808d7e9fee99bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a595583b5c917fe33aed",
+    "url": "/static/css/main.5aabb151.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a595583b5c917fe33aed",
+    "url": "/static/js/main.1b22e936.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
